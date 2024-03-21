e923d1295d2fbc5d4a3617d0ed661fff275d0301
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 18:08:29 -0300

    exercicio03

---
+++ exercicio03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor de A: ");
+        int A = scanner.nextInt();
+
+        System.out.print("Digite o valor de B: ");
+        int B = scanner.nextInt();
+
+        if (A > B) {
+            System.out.println("A é maior que B.");
+        } else if (B > A) {
+            System.out.println("B é maior que A.");
+        } else {
+            System.out.println("A sequência de números informada é inválida, pois os números são iguais.");
+        }
+
+        scanner.close();
+    }
+}
+
