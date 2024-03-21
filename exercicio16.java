0ec004b29bb33c4d850321af47c5fb17868d49b6
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 19:18:29 -0300

    exercicio16

---
+++ exercicio16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class exercicio16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+         double nota01 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+         double nota02 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+         double nota03 = scanner.nextDouble();
+
+        double media = (nota01 + nota02 + nota03) / 3;
+       
+         System.out.println("A média aritmética das notas é: " + media);
+
+        scanner.close();
+    }
+}
