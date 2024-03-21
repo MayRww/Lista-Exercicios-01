5255b2d1e9cec0f43a7ec850a7da19dc0253829e
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 18:15:31 -0300

    exercicio05

---
+++ exercicio05.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class exercicio05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor de A: ");
+        int A = scanner.nextInt();
+
+        System.out.print("Digite o valor de B: ");
+        int B = scanner.nextInt();
+
+        System.out.println("Valores originais:");
+        System.out.println("A: " + A);
+        System.out.println("B: " + B);
+
+        // Troca dos valores sem variável temporária
+        A = A + B;
+        B = A - B;
+        A = A - B;
+
+        System.out.println("\nValores trocados:");
+        System.out.println("A: " + A);
+        System.out.println("B: " + B);
+
+        scanner.close();
+    }
+}
