882bca2c39d27c4d7b4db04709e6df0f72fc913a
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 18:35:01 -0300

    exercicio10'

---
+++ exercicio10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número de 1 a 5: ");
+        int numero = scanner.nextInt();
+
+        if (numero == 1) {
+            System.out.println("Um");
+        } else if (numero == 2) {
+            System.out.println("Dois");
+        } else if (numero == 3) {
+            System.out.println("Três");
+        } else if (numero == 4) {
+            System.out.println("Quatro");
+        } else if (numero == 5) {
+            System.out.println("Cinco");
+        } else {
+            System.out.println("Número inválido!");
+        }
+
+        scanner.close();
+    }
+}
