fb29a7bc6fa8c8ef57a484e474ecd066749ce5e9
Mayara <mayara.wojceikovski@gmail.com>
2024-03-21 18:29:16 -0300

    exercicio09

---
+++ exercicio09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercicio09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor de A: ");
+        int A = scanner.nextInt();
+
+        System.out.print("Digite o valor de B: ");
+        int B = scanner.nextInt();
+
+        if (A == B) {
+            System.out.println("Os números são iguais.");
+        } else {
+            System.out.println("Os números são diferentes.");
+            if (A > B) {
+                System.out.println("O número A é maior que o número B.");
+            } else {
+                System.out.println("O número B é maior que o número A.");
+            }
+        }
+
+        scanner.close();
+    }
+}
